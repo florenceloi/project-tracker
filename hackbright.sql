a92574fd8ef371ddd22fde3a73af524e909cccde
Florence Loi <florence.c.loi@gmail.com>
2016-01-25 17:57:18 -0800

    added actual file...

--- hackbright.sql
+++ hackbright.sql
@@ -23,6 +23,155 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: grades; Type: TABLE; Schema: public; Owner: user; Tablespace: 
+--
+
+CREATE TABLE grades (
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO "user";
+
+--
+-- Name: projects; Type: TABLE; Schema: public; Owner: user; Tablespace: 
+--
+
+CREATE TABLE projects (
+    id integer NOT NULL,
+    title character varying(30),
+    description text,
+    max_grade integer
+);
+
+
+ALTER TABLE public.projects OWNER TO "user";
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE; Schema: public; Owner: user
+--
+
+CREATE SEQUENCE projects_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.projects_id_seq OWNER TO "user";
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: user
+--
+
+ALTER SEQUENCE projects_id_seq OWNED BY projects.id;
+
+
+--
+-- Name: students; Type: TABLE; Schema: public; Owner: user; Tablespace: 
+--
+
+CREATE TABLE students (
+    first_name character varying(30),
+    last_name character varying(30),
+    github character varying(30) NOT NULL
+);
+
+
+ALTER TABLE public.students OWNER TO "user";
+
+--
+-- Name: reportcardview; Type: VIEW; Schema: public; Owner: user
+--
+
+CREATE VIEW reportcardview AS
+ SELECT students.first_name,
+    students.last_name,
+    projects.title,
+    projects.max_grade,
+    grades.grade
+   FROM ((students
+     JOIN grades ON (((students.github)::text = (grades.student_github)::text)))
+     JOIN projects ON (((projects.title)::text = (grades.project_title)::text)));
+
+
+ALTER TABLE public.reportcardview OWNER TO "user";
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: user
+--
+
+ALTER TABLE ONLY projects ALTER COLUMN id SET DEFAULT nextval('projects_id_seq'::regclass);
+
+
+--
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: user
+--
+
+COPY grades (student_github, project_title, grade) FROM stdin;
+jhacks	Markov	10
+jhacks	Blockly	2
+sdevelops	Blockly	100
+sdevelops	Markov	50
+\.
+
+
+--
+-- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: user
+--
+
+COPY projects (id, title, description, max_grade) FROM stdin;
+1	Markov	Tweets generated from Markov chains	50
+2	Blockly	Programmatic Logic Puzzle Game	10
+3	Calculator	Built a calculator	20
+4	Shopping site	Bought melons	100
+5	Restaurant Ratings	Print ratings using dictionaries	20
+\.
+
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
+--
+
+SELECT pg_catalog.setval('projects_id_seq', 5, true);
+
+
+--
+-- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: user
+--
+
+COPY students (first_name, last_name, github) FROM stdin;
+Jane	Hacker	jhacks
+Sarah	Developer	sdevelops
+\.
+
+
+--
+-- Name: projects_pkey; Type: CONSTRAINT; Schema: public; Owner: user; Tablespace: 
+--
+
+ALTER TABLE ONLY projects
+    ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: students_pkey; Type: CONSTRAINT; Schema: public; Owner: user; Tablespace: 
+--
+
+ALTER TABLE ONLY students
+    ADD CONSTRAINT students_pkey PRIMARY KEY (github);
+
+
 --
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
